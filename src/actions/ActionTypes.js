a74e3fe8ad5fefd48af72c92994971c517f358e1
Manish <makkanimation@gmail.com>
2018-01-17 13:52:13 +0530

    made Alert Component but not working on second time

--- src/actions/ActionTypes.js
+++ src/actions/ActionTypes.js
@@ -3,3 +3,4 @@ export const REQ_DATA = '_REQ_DATA';
 export const RECV_DATA = '_RECV_DATA';
 export const ALERT_SUCCESS = 'ALERT_SUCCESS';
 export const ALERT_FAIL = 'ALERT_FAIL';
+export const ALERT_WARNING = 'ALERT_WARNING';
--- src/components/EditUserComponent.jsx
+++ src/components/EditUserComponent.jsx
@@ -8,6 +8,7 @@ import SelectBox from './global/SelectBox';
 import TextArea from './global/TextArea';
 import Button from './global/Button';
 import {ALERT_SUCCESS, ALERT_FAIL } from '../actions/ActionTypes'
+import AlertMsg from './global/AlertMsg';
 
 export default class EditUserComponent extends React.Component{
     constructor(props){
@@ -15,7 +16,7 @@ export default class EditUserComponent extends React.Component{
     }
     render(){
         if(this.props.userRecords.length==0){
-            return '';
+            return null;
         }
         return(
             <div className="container-fluid">
@@ -25,26 +26,19 @@ export default class EditUserComponent extends React.Component{
                     <h1 className="center-align">Edit User</h1>
                     <form className='formcls' action=""  onSubmit={this.props.isSubmitForm}>
                     <div className="container">
-                        {this.props.alertType==ALERT_SUCCESS && <div className="alert alert-success">
-                            <strong>Success!</strong> {this.props.alertMsg}
-                            </div>
-                        }
-                        {this.props.alertType==ALERT_FAIL && <div className="alert alert-danger">
-                            <strong>Fail!</strong> {this.props.alertMsg}
-                            </div>
-                        }
+                        <AlertMsg alertType={this.props.alertType} alertMsg={this.props.alertMsg}/>
 
                         <UploadPic srcUrl={this.props.userRecords.avatar} />
                         <Input label="First Name" placeHolder="Enter First Name" name="fname" RequiredField={true} value={this.props.userRecords.first_name}/>
                         <Input label="Last Name" placeHolder="Enter Last Name" name="lname" RequiredField={true}  value={this.props.userRecords.last_name} />
-                        <Input label="Designation" placeHolder="Enter Designation" name="designation" RequiredField={true}  value='' />
-                        <Input label="Email" inputType="email" placeHolder="Enter Email" name="email" RequiredField={true} />
-                        <Input label="Phone No" placeHolder="Enter Phone No" name="phoneno" RequiredField={true} />
-                        <Input label="Address" placeHolder="Enter Your Address" name="address" RequiredField={true} />
-                        <Input label="Zip Code" placeHolder="Enter Zip Code" name="zip" RequiredField={true} />
+                        <Input label="Designation" placeHolder="Enter Designation" name="designation"   value='' />
+                        <Input label="Email" inputType="email" placeHolder="Enter Email" name="email" />
+                        <Input label="Phone No" placeHolder="Enter Phone No" name="phoneno"  />
+                        <Input label="Address" placeHolder="Enter Your Address" name="address"  />
+                        <Input label="Zip Code" placeHolder="Enter Zip Code" name="zip"  />
                         <CheckBoxOrRadio inputType="radio" label="Gender" name="gender" option={['Male','Female']} />
                         <SelectBox label="Country" placeHolder="Select Country" name="country" option={["India","USA","Canada"]}/>
-                        <TextArea label="Tell me About Yourself" placeHolder="Tell me About Yourself" name="selfDescription" RequiredField={true} value="" />
+                        <TextArea label="Tell me About Yourself" placeHolder="Tell me About Yourself" name="selfDescription" value="" />
                         <div className="clearfix">
                         <Button inputType="submit" clsName="signupbtn" name="signup" value="Submit" />
                         <Button inputType="button" clsName="cancelbtn" name="back" value="Back" />
@@ -67,5 +61,5 @@ EditUserComponent.propTypes = {
     userRecords: PropTypes.array.isRequired,
     isSubmitForm:PropTypes.func,
     alertType: PropTypes.string,
-    alertMsg: PropTypes.string,
+    alertMsg: PropTypes.string
 }
